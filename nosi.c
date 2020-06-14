67ca77d6ff13ad5c1625bb75d5f6035f79340aa2
Japanese-Dashi <66872231+Japanese-Dashi@users.noreply.github.com>
2020-06-15 02:02:38 +0900

    Add files via upload

---
+++ nosi.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+#define ZZZZZZ void
+#define ZZZZZ2 main
+#define ZZZZ2Z (
+#define ZZZ2ZZ )
+#define ZZ2ZZZ {
+#define Z2ZZZZ }
+#define ZZZZ22 printf
+#define ZZZ22Z "頭壊れるぅ!!\n"
+#define ZZ22ZZ ;
+
+ZZZZZZ ZZZZZ2 ZZZZ2Z ZZZZZZ ZZZ2ZZ ZZ2ZZZ
+ZZZZ22 ZZZZ2Z ZZZ22Z ZZZ2ZZ ZZ22ZZ
+Z2ZZZZ
