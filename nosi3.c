d2ea9f358dad0aacdafd276d52d77dab77236cde
Japanese-Dashi <66872231+Japanese-Dashi@users.noreply.github.com>
2020-06-23 20:50:49 +0900

    Add files via upload

---
+++ nosi3.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define llllllll void
+#define lllllll1 int
+#define llllll1l main
+#define lllll1ll kaijo
+#define llll1lll (
+#define lll1llll )
+#define ll1lllll {
+#define l1llllll }
+#define llllll11 ;
+#define lllll11l printf
+#define llll11ll scanf
+#define lll11lll "入力された数字の階乗を表示します。\n"
+#define ll11llll "%d"
+#define l11lllll ,
+#define lllll111 "%dの階乗は%dです。\n"
+#define llll111l =
+#define lll111ll >
+#define ll111lll *
+#define l111llll -
+#define llll1111 1
+#define lll1111l if
+#define ll1111ll else
+#define l1111lll return
+#define lll11111 &
+
+
+lllllll1 lllll1ll llll1lll lllllll1 lll1llll llllll11
+
+llllllll llllll1l llll1lll llllllll lll1llll ll1lllll
+    lllll11l llll1lll lll11lll lll1llll llllll11
+    lllllll1 id llllll11
+    llll11ll llll1lll ll11llll l11lllll lll11111 id lll1llll llllll11
+
+    lllllll1 answer llll111l lllll1ll llll1lll id lll1llll llllll11
+    lllll11l llll1lll lllll111 l11lllll id l11lllll answer lll1llll llllll11
+l1llllll
+
+lllllll1 lllll1ll llll1lll lllllll1 idc lll1llll ll1lllll
+    lll1111l llll1lll idc lll111ll llll1111 lll1llll ll1lllll
+        l1111lll idc ll111lll lllll1ll llll1lll idc l111llll llll1111 lll1llll llllll11
+    l1llllll
+    ll1111ll ll1lllll
+        l1111lll llll1111 llllll11
+    l1llllll
+l1llllll
